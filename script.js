d544a6fb61e5b13fc883b8561248819ac43cde89
Jeremias Enriquez <capuche777@gmail.com>
2018-02-21 15:38:22 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var nombre = document.getElementByID(txt_nombre).value;
+
+var pass = document.getElementByID(pass_contra).value;
+
+function ValidarFormulario() {
+  
+}
+
+function validarNombre () {
+  
+}
