5b62947e2cf73812d89c4ec4e4275a5968369b0c
Jeremias Enriquez <capuche777@gmail.com>
2018-02-21 16:31:05 -0600

    arreglar funcion de validar

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var nombre = document.getElementByID(txt_nombre).value;
+const numeros = [0,1,2,3,4,5,6,7,8,9];
 
-var pass = document.getElementByID(pass_contra).value;
+var nombre = document.getElementByID('txt_nombre').value;
+
+var pass = document.getElementByID('pass_contra').value;
 
 function ValidarFormulario() {
   
 }
 
-function validarNombre () {
-  
+function validarNombre (nombre) {
+        alert('Ingrese solo letras');
 }
