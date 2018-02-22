8b77db726bb2677309a320246e3ef9a49e6b8f9d
Jeremias Enriquez <capuche777@gmail.com>
2018-02-22 16:27:17 -0600

    name validation is fixed

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const alfabeto = /[a-zA-Z]/;
-
-var nombre = document.getElementByID('txt_nombre').value;
-
-var pass = document.getElementByID('pass_contra').value;
+var pass = document.getElementById('pass_contra').value;
 
 function ValidarFormulario() {
   
 }
 
-function validarNombre (nombre) {
-        if (!=) {
-
+function validarNombre() {
+        var _nombre = document.getElementById('txt_nombre').value;
+        if (_nombre.length > 0) {
+                _nombre = _nombre.split('');
+                for (var i = 0; i < _nombre.length; i++) {
+                        var codigo = _nombre[i].charCodeAt(0);
+                        if (!((codigo >= 97 && codigo <= 122)
+                          || (codigo >= 65 && codigo <= 90)
+                          || (codigo == 32))) {
+                                  alert('solo se permiten letras y espacios');
+                          }
+                }
+        } else {
+                return 'El nombre no puede estar vacio';
         }
+        return 'Nombre correcto';
 }
